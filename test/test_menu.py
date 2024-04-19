933dfd5c7e0cae3cd4d6d6f9aaa8f90749318648
Scrudgie-Meyer <wofer2018@gmail.com>
2024-04-19 13:05:04 +0300

    Delete extra files

--- test/test_menu.py
+++
@@ -1,5 +0,0 @@
-from lib.enums.game_events import NEXT_STAGE
-import pygame
-
-mouse_click = pygame.event.post(pygame.event.Event(pygame.MOUSEBUTTONDOWN, {"mouse_pos": (100, 100)}))
-assert pygame.event.poll(), mouse_click
